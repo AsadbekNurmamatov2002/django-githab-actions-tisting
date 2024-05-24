52e1d9f7d35f5cd9c8c0a5d0168c07f370c40c3a
AsadbekNurmamtov2002 <nurmamatovasadbek85@gmail.com>
2024-05-25 04:08:30 +0500

    my test

--- app/users/usermanagers.py
+++ app/users/usermanagers.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager
-from .models import User
+from django.contrib.auth.models import User
 
 
 class MyUserManagers(BaseUserManager):
